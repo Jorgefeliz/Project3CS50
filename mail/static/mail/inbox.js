f16457eb4a613cf2bddde78ee4934aa000116541
jorgefeliz <jorgefelizv@gmail.com>
2021-10-16 19:32:46 -0400

    almost view email

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  //document.querySelector(".read").addEventListener('click', () => show_one_email());
+ 
+  
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -14,6 +17,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-read').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -59,12 +63,14 @@ function load_mailbox(mailbox) {
 
 
   // Show the mailbox and hide other views
+  document.querySelector('#email-read').style.display = 'none';
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
+  document.querySelector('#emails-view').textContent = "";
   if (mailbox === "sent"){
     show_emails(mailbox);
   
@@ -81,7 +87,7 @@ function load_mailbox(mailbox) {
 }
 
 function show_emails(mailbox){
-  document.querySelector('#emails-view').textContent = "";
+  
 
   let route = '/emails/';
   route = route + mailbox;
@@ -93,11 +99,13 @@ function show_emails(mailbox){
     console.log(mailbox);
  
     // ... do something else with emails ...
+
+    
+
     emails.forEach(email => { 
       console.log(email.id);
 
       const email_view = document.getElementById('emails-view');
-
       let myDiv = document.createElement('div');
       myDiv.setAttribute("id", "myDiv");
       const strong = document.createElement('strong');
@@ -117,17 +125,57 @@ function show_emails(mailbox){
         myDiv.setAttribute("style", "background-color: gray");
       }
      
-      /*
-      myDiv.innerHTML = `<a href="http://127.0.0.1:8000/emails/${email.id}" >
-                        <strong> ${email.sender} </strong> <em> ${email.subject} </em> <i> ${email.timestamp} </i>
-                        </a>`;
-      */
+      // `<a class="email" href="http://127.0.0.1:8000/emails/${email.id}" > </a>
+      myDiv.innerHTML = `<strong> ${email.sender} </strong> <em> ${email.subject} </em> <i> ${email.timestamp} </i>
+                        <input type="button" value="Read" onclick="show_one_email(${email.id});" />
+                        <input type="button" value="Archive" onclick="archive_email(${email.id});" />`;
+      
 
-      myDiv.append(strong,' ', em, ' ', time);
+      //myDiv.append(strong,' ', em, ' ', time);
       email_view.append(myDiv);
       
     });
 
+   
   });
+
+}
+
+function show_one_email(email_id){
+  document.querySelector('#email-read').style.display = 'block';
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+//-----------------------------------------------------------
+
+let route = "/emails/" + email_id;
+fetch(route)
+.then(response => response.json())
+.then(email => {
+    // Print email
+    console.log(email_id);
+
+      
+    let email_read = document.getElementById('email-read');
+    let myDiv = document.createElement('div');
+    myDiv.setAttribute("id", "myDiv_read");
+    myDiv.innerHTML = `<div class="form-group">
+                        <small> ${email.timestamp} </small><br>
+                        From: <input disabled class="form-control" value="${email.sender}">
+                        
+                        To: <input disabled class="form-control" value="${email.recipients}">
+
+                        Subject: <p disable class="form-control"  >${email.subject}</p>
+                         </div>
+                        Body:
+                      <p disable class="form-control"  >${email.body}</p>`;
+    email_read.append(myDiv);
+});
+
+fetch(route, {
+  method: 'PUT',
+  body: JSON.stringify({
+      read: true
+  })
+})
 
 }
